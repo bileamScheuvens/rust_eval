bcb4d9bbfd2fb5a3d43d4f97faee91c9251f6052
TK <t.kuehn@oneup-digital.com>
2023-06-20 14:11:59 +0200

    added display trait to print values of result

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use anyhow::Result;
 
 use crate::lexer::lexer::{Lexer, Token};
@@ -18,13 +20,24 @@ pub enum ResultEval {
     Bool(bool),
 }
 
+impl fmt::Display for ResultEval {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ResultEval::Int(value) => write!(f, "{:?}", value),
+            ResultEval::Bool(value) => write!(f, "{:?}", value),
+        }
+    }
+}
+
 pub struct ShuntiyardParser {
+    lexer: Lexer,
     operator_stack: Vec<Token>,
     output_queue: Vec<ASTNode>,
 }
 impl ShuntiyardParser {
-    pub fn new() -> ShuntiyardParser {
+    pub fn new(lexer: Lexer) -> ShuntiyardParser {
         let parser = ShuntiyardParser {
+            lexer: lexer,
             operator_stack: Vec::new(),
             output_queue: Vec::new(),
         };
@@ -34,7 +47,7 @@ impl ShuntiyardParser {
     pub fn check_for_zero(&self, l_node: ASTNode, r_node: ASTNode) -> ASTNode {
         // check if left or right side of multiplication is 0 to simply evaluation
         if (l_node == ASTNode::Number(0)) | (r_node == ASTNode::Number(0)) {
-            println!("One side of tree is zero");
+            //println!("One side of tree is zero");
             return ASTNode::Number(0);
         } else {
             return ASTNode::Multiply(Box::new(l_node), Box::new(r_node));
@@ -54,9 +67,8 @@ impl ShuntiyardParser {
         self.output_queue.push(node);
     }
 
-    pub fn parse(&mut self, input: String) -> Result<ASTNode> {
-        let mut lexer = Lexer::new(input);
-        while let Ok(token) = lexer.next_token() {
+    pub fn parse(&mut self) -> Result<ASTNode> {
+        while let Ok(token) = self.lexer.next_token() {
             match token {
                 Token::Zero | Token::One => self
                     .output_queue
@@ -94,20 +106,20 @@ impl ShuntiyardParser {
                 },
                 Token::Eof => break,
             }
-            println!(
-                "Current Token {:?} & Current Stack {:?} & Current output queue {:?}",
-                token, self.operator_stack, self.output_queue
-            )
+            // println!(
+            //     "Current Token {:?} & Current Stack {:?} & Current output queue {:?}",
+            //     token, self.operator_stack, self.output_queue
+            // )
         }
         while self.operator_stack.len() > 0 {
             // Pop them off and push them to the output_queue
             let op = &self.operator_stack.pop().unwrap();
             self.add_node(op);
         }
-        println!(
-            "End Stack {:?} & End output queue {:?}",
-            self.operator_stack, self.output_queue
-        );
+        // println!(
+        //     "End Stack {:?} & End output queue {:?}",
+        //     self.operator_stack, self.output_queue
+        // );
         Ok(self.output_queue.pop().unwrap())
     }
 }
